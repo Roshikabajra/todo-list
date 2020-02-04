8071895e78dedbc7a0c6d117ab3824fb601f2e85
Roshika Bajracharya <roshikabajracharyaa@gmail.com>
2020-02-04 16:08:09 +0545

    this is a todo list

---
+++ app.js
@@ -0,0 +1,2 @@
+var taskInput=document.getElementById("new-task");//Add a new task.
+var addButton=document.getElementsByTagName("button")[0];//first button
---
+++ todo.js
@@ -0,0 +1,7 @@
+document.getElementById("addItem").onclick = function() {
+  var node = document.createElement("Li");
+  var text = document.getElementById("user_input").value;
+  var textnode = document.createTextNode(text);
+  node.appendChild(textnode);
+  document.getElementById("list_item").appendChild(node);
+};
